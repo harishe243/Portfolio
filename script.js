5a486ce0bae441b6e61aa2ef38a801c21e51ddc0
HARISH E <harishe243@gmail.com>
2025-02-28 11:18:31 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,16 @@
+ // document.querySelectorAll('.navbar a').forEach(anchor => {
+//     anchor.addEventListener('click', function (e) {
+//         e.preventDefault();
+
+//         const targetId = this.getAttribute('#project').substring(1);
+//         const targetElement = document.getElementById(project);
+
+//         // Smooth scroll to the target element
+//         window.scrollTo({
+//             top: targetElement.offsetTop - 80, // Adjust offset for navbar height
+//             behavior: 'smooth'
+//         });
+//     });
+// });
+
+
